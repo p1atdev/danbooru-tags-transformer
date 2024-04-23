66d862a3904b3c943a49c21f2f59fb58be634c49
Plat <git@p1at.dev>
2024-04-23 20:17:54 +0900

    fix: swap height and width

--- tests/test_aspect_ratio.py
+++ tests/test_aspect_ratio.py
@@ -7,17 +7,17 @@ from src.aspect_ratio import calculate_aspect_ratio_tag
 
 
 def test_aspect_ratio():
-    assert calculate_aspect_ratio_tag(768, 380) == "<|aspect_ratio:ultra_tall|>"
-    assert calculate_aspect_ratio_tag(768, 384) == "<|aspect_ratio:ultra_tall|>"
+    assert calculate_aspect_ratio_tag(380, 768) == "<|aspect_ratio:ultra_tall|>"
+    assert calculate_aspect_ratio_tag(384, 768) == "<|aspect_ratio:ultra_tall|>"
 
-    assert calculate_aspect_ratio_tag(768, 432) == "<|aspect_ratio:tall|>"
-    assert calculate_aspect_ratio_tag(1024, 768) == "<|aspect_ratio:tall|>"
+    assert calculate_aspect_ratio_tag(432, 768) == "<|aspect_ratio:tall|>"
+    assert calculate_aspect_ratio_tag(768, 1024) == "<|aspect_ratio:tall|>"
 
     assert calculate_aspect_ratio_tag(768, 768) == "<|aspect_ratio:square|>"
-    assert calculate_aspect_ratio_tag(640, 600) == "<|aspect_ratio:square|>"
+    assert calculate_aspect_ratio_tag(600, 640) == "<|aspect_ratio:square|>"
 
-    assert calculate_aspect_ratio_tag(768, 1024) == "<|aspect_ratio:wide|>"
-    assert calculate_aspect_ratio_tag(768, 1366) == "<|aspect_ratio:wide|>"
+    assert calculate_aspect_ratio_tag(1024, 768) == "<|aspect_ratio:wide|>"
+    assert calculate_aspect_ratio_tag(1366, 768) == "<|aspect_ratio:wide|>"
 
-    assert calculate_aspect_ratio_tag(768, 1536) == "<|aspect_ratio:ultra_wide|>"
-    assert calculate_aspect_ratio_tag(768, 2048) == "<|aspect_ratio:ultra_wide|>"
+    assert calculate_aspect_ratio_tag(1536, 768) == "<|aspect_ratio:ultra_wide|>"
+    assert calculate_aspect_ratio_tag(2048, 768) == "<|aspect_ratio:ultra_wide|>"
