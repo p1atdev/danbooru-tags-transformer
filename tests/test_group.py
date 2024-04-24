24f444cc1b39293f53bec4324092a9f9b3d0ff73
Plat <git@p1at.dev>
2024-04-24 20:25:32 +0900

    chore: update tag group testing

--- tests/test_group.py
+++ tests/test_group.py
@@ -15,20 +15,26 @@ def test_people_tags():
     group = TagGroup()
 
     assert len(group.people_tags) > 0
+    assert "1girl" in group.people_tags
+    assert "6+girls" in group.people_tags
 
 
 def test_watermark_tags():
     group = TagGroup()
 
     assert len(group.watermark_tags) > 0
+    assert "watermark" in group.watermark_tags
 
 
 def test_artistic_error_tags():
     group = TagGroup()
+
     assert len(group.artistic_error_tags) > 0
+    assert "bad anatomy" in group.artistic_error_tags
 
 
 def test_focus_tags():
     group = TagGroup()
 
     assert len(group.focus_tags) > 0
+    assert "solo focus" in group.focus_tags
